6585490fe7ac122fd7d77cc48e24d8d9aa79b9b6
dreamheart <leondreamheart@outlook.com>
2021-07-09 14:44:39 -0400

    added Todd's CSS Style

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -271,20 +271,23 @@ function displayRecipe(event) {
 
 	// Create the elements needed for the display of the recipe
 	let recipeName = document.createElement("h2");
+	let divImg1 = document.createElement("div");
 	let recipeImage = document.createElement("img");
 	let recipeSummary = document.createElement("p");
 
 	// Assign values to the elements and format them
 	recipeName.textContent = recipeSelected.title;
 	recipeName.setAttribute("class", "has-text-centered")
+	divImg1.setAttribute("class", "column");
 	recipeImage.setAttribute("src", recipeSelected.image);
 	recipeImage.setAttribute("class", "column");
 	recipeSummary.innerHTML = recipeInfo[0].summary;
-
+	
 	// Select the div that will hold the recipe selected and append to page
 	let recipeSelectedDiv = document.querySelector(".recipe-selected");
+	divImg1.append(recipeImage);
 	recipeSelectedDiv.append(recipeName);
-	recipeSelectedDiv.append(recipeImage);
+	recipeSelectedDiv.append(divImg1);
 	recipeSelectedDiv.append(recipeSummary);
 
 	// Removes the is-hidden class only from the selected recipe div
