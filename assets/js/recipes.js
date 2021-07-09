79818e6f93d72dcf039e529baad10f68a6f2d1e8
Anthony Gress <the.techx.guy@gmail.com>
2021-07-08 20:40:48 -0400

    refactored input html and js, added a few new helper functions

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -35,7 +35,7 @@ function handleSearchInput (){
 		appendIngredients();
 	}
 }
-
+// adds ingredients to page
 function appendIngredients(){
 	// clear list
 	ingredientList.innerHTML = "";
@@ -60,7 +60,7 @@ function appendIngredients(){
 
 
 }
-
+// removes the ingredient that the user clicked on
 function removeIngredient(event){
 	let element = event.target;
 
@@ -76,7 +76,7 @@ function removeIngredient(event){
 
 	}
 };
-
+// makes it so the enter key adds the ingredient to the list
 function onEnterKey (event){
 		if (event.key === 'Enter') {
 			// Cancel the default action, if needed
@@ -93,6 +93,7 @@ function storeIngredients() {
   // save ingredients array to local storage
   localStorage.setItem("ingredients", JSON.stringify(ingredients));
 }
+
 // Will search for a max of 5 recipes by ingredients
 function searchRecipeByIngredients(){
 var apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" +
@@ -128,42 +129,6 @@ fetch(apiUrl, {
 });
 // TODO Uncomment when ready to go live
 //fillSuggestedRecipes();
-}
-
-function searchRecipeByIngredients() {
-	var apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" +
-		"ingredients=" + ingredient1 +
-		"%2C" + ingredient2 +
-		"%2C" + ingredient3 +
-		"%2C" + ingredient4 +
-		"&number=5&ignorePantry=true&ranking=1";
-
-	fetch(apiUrl, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "9aa92d7aa7msh77d7072478c9634p1b415cjsn6487488da0d0",
-			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-		}
-	})
-		.then(function (response) {
-			// Will return Json object if the request is successful
-			if (response.status == 200) {
-				return response.json();
-			}
-		})
-		// TODO: Add logic for data manipulation
-		.then(function (data) {
-			console.log(apiUrl);
-			console.log(data);
-			// set global var to JSON object
-			recipeData1 = data;
-		})
-		// Error handler
-		.catch(function (err) {
-			console.error(err);
-		});
-	// TODO Uncomment when ready to go live
-	//fillSuggestedRecipes();
 }
 
 function fillSuggestedRecipes() {
