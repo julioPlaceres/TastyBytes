be2cce3d9425629b4878824073103e4e92590f8f
dreamheart <leondreamheart@outlook.com>
2021-07-09 11:39:13 -0400

    Added units to the main recipe, by creating a for loop to check which of the units of measurement belong to missed ingredients and the ones the user does have

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -21,10 +21,10 @@ console.log("Initial API call");
 console.log(recipeData);
 console.log("Detailed recipe info");
 console.log(recipeInfo);
-function handleSearchInput (){
+function handleSearchInput() {
 	// get ingredient input value
 	let searchInputVal = searchInputEl.value.trim();
-	if (searchInputVal != ""){
+	if (searchInputVal != "") {
 		// push ingredient to array
 		ingredients.push(searchInputVal);
 		// clear input box
@@ -36,7 +36,7 @@ function handleSearchInput (){
 	}
 }
 // adds ingredients to page
-function appendIngredients(){
+function appendIngredients() {
 	// clear list
 	ingredientList.innerHTML = "";
 	// loop through the array and put everything on the page
@@ -61,74 +61,74 @@ function appendIngredients(){
 
 }
 // removes the ingredient that the user clicked on
-function removeIngredient(event){
+function removeIngredient(event) {
 	let element = event.target;
 
-  // Checks if element is a button
-  if (element.matches("button") === true) {
-    // Get its data-index value and remove the ingredient from the list
-    let index = element.parentElement.parentElement.getAttribute("data-index");
+	// Checks if element is a button
+	if (element.matches("button") === true) {
+		// Get its data-index value and remove the ingredient from the list
+		let index = element.parentElement.parentElement.getAttribute("data-index");
 		// delete this one ingredient that we clicked
-    ingredients.splice(index, 1);
-    // store ingredients in local storage and write to page
-    storeIngredients();
-    appendIngredients();
+		ingredients.splice(index, 1);
+		// store ingredients in local storage and write to page
+		storeIngredients();
+		appendIngredients();
 
 	}
 };
 // makes it so the enter key adds the ingredient to the list
-function onEnterKey (event){
-		if (event.key === 'Enter') {
-			// Cancel the default action, if needed
-			event.preventDefault();
-			// Trigger the button element with a click
-			addIngredientBtn.click();
-			// reset input text on search
-			searchInputEl.value = "";
-		}
-		
-	};
+function onEnterKey(event) {
+	if (event.key === 'Enter') {
+		// Cancel the default action, if needed
+		event.preventDefault();
+		// Trigger the button element with a click
+		addIngredientBtn.click();
+		// reset input text on search
+		searchInputEl.value = "";
+	}
+
+};
 
 function storeIngredients() {
-  // save ingredients array to local storage
-  localStorage.setItem("ingredients", JSON.stringify(ingredients));
+	// save ingredients array to local storage
+	localStorage.setItem("ingredients", JSON.stringify(ingredients));
 }
 
 // Will search for a max of 5 recipes by ingredients
-function searchRecipeByIngredients(){
-var apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" +
-"ingredients=" + ingredient1 + 
-"%2C" + ingredient2 + 
-"%2C" + ingredient3 + 
-"%2C" + ingredient4 +  
-"&number=5&ignorePantry=true&ranking=1";
+function searchRecipeByIngredients() {
+	var apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?" +
+		"ingredients=" + ingredient1 +
+		"%2C" + ingredient2 +
+		"%2C" + ingredient3 +
+		"%2C" + ingredient4 +
+		"&number=5&ignorePantry=true&ranking=1";
 
-fetch(apiUrl, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "9aa92d7aa7msh77d7072478c9634p1b415cjsn6487488da0d0",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-})
-.then(function (response) {
-	// Will return Json object if the request is successful
-	if(response.status == 200){
-		return response.json();
-	}
-})
-// TODO: Add logic for data manipulation
-.then(function (data) {
-	console.log(apiUrl);
-  console.log(data);
-	// set global var to JSON object TODO rename
-	recipeData1 = data;
-})
-// Error handler
-.catch(function (err) {
-	console.error(err);
-});
-// TODO Uncomment when ready to go live
-//fillSuggestedRecipes();
+	fetch(apiUrl, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "9aa92d7aa7msh77d7072478c9634p1b415cjsn6487488da0d0",
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+		}
+	})
+		.then(function (response) {
+			// Will return Json object if the request is successful
+			if (response.status == 200) {
+				return response.json();
+			}
+		})
+		// TODO: Add logic for data manipulation
+		.then(function (data) {
+			console.log(apiUrl);
+			console.log(data);
+			// set global var to JSON object TODO rename
+			recipeData1 = data;
+		})
+		// Error handler
+		.catch(function (err) {
+			console.error(err);
+		});
+	// TODO Uncomment when ready to go live
+	//fillSuggestedRecipes();
 }
 
 function fillSuggestedRecipes() {
@@ -155,9 +155,9 @@ function columnLayout(title, recipePicUrl, i) {
 	img.setAttribute("data-id", recipeData[i].id);
 	img.setAttribute("class", "roundedCorners mt-5");
 	// seat attribute on divs in this order for bulma css columns layout
-	div1.setAttribute("class","column has-text-centered is-size-5 has-text-weight-semibold");
-	div2.setAttribute("class","columns");
-	div3.setAttribute("class","column");
+	div1.setAttribute("class", "column has-text-centered is-size-5 has-text-weight-semibold");
+	div2.setAttribute("class", "columns");
+	div3.setAttribute("class", "column");
 	div1.setAttribute("class", "column has-text-centered divLayout");
 	div2.setAttribute("class", "columns divLayout");
 	div3.setAttribute("class", "column divLayout");
@@ -173,36 +173,36 @@ function getRecipeInfo() {
 	//TODO refactor for loop through JSON array and set ID = to recipe1, recipe 2 etc.
 	let recipe1 = 560113;
 	let apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=" + recipe1 + "&includeNutrition=true";
-//	+ "%2C" + recipe2 + 
-// "%2C" + recipe3 + 
-// "%2C" + recipe4 +  
+	//	+ "%2C" + recipe2 + 
+	// "%2C" + recipe3 + 
+	// "%2C" + recipe4 +  
 
-fetch(apiUrl, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "9aa92d7aa7msh77d7072478c9634p1b415cjsn6487488da0d0",
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	}
-})
-.then(function (response) {
-	// Will return Json object if the request is successful
-	if(response.status == 200){
-		console.log(response);
-		return response.json();
-	}
-})
-// TODO: Add logic for data manipulation
-.then(function (data) {
-	console.log(apiUrl);
-  console.log(data);
-	// set global var to JSON object rename later
-	nutritionData1 = data;
-	console.log(nutritionData1);
-})
-// Error handler
-.catch(function (err) {
-	console.error(err);
-});
+	fetch(apiUrl, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "9aa92d7aa7msh77d7072478c9634p1b415cjsn6487488da0d0",
+			"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+		}
+	})
+		.then(function (response) {
+			// Will return Json object if the request is successful
+			if (response.status == 200) {
+				console.log(response);
+				return response.json();
+			}
+		})
+		// TODO: Add logic for data manipulation
+		.then(function (data) {
+			console.log(apiUrl);
+			console.log(data);
+			// set global var to JSON object rename later
+			nutritionData1 = data;
+			console.log(nutritionData1);
+		})
+		// Error handler
+		.catch(function (err) {
+			console.error(err);
+		});
 	//	+ "%2C" + recipe2 + 
 	// "%2C" + recipe3 + 
 	// "%2C" + recipe4 +  
@@ -268,7 +268,7 @@ function displayRecipe(event) {
 
 	// Assign values to the elements and format them
 	recipeName.textContent = recipeSelected.title;
-	recipeName.setAttribute("class", "has-text-centered")	
+	recipeName.setAttribute("class", "has-text-centered")
 	recipeImage.setAttribute("src", recipeSelected.image);
 	recipeImage.setAttribute("class", "column");
 
@@ -293,8 +293,23 @@ function displayRecipe(event) {
 		let amount = recipeSelected.missedIngredients[i].amount;
 		let productName = recipeSelected.missedIngredients[i].name;
 
+		// Will be use to loop through the ingredients of the second API call
+		// to get the missing ingredient index
+		let ingredientIndex;
+		//Find the index of the ingredient missing
+		for (let a = 0; a < recipeInfo[0].nutrition.ingredients.length; a++) {
+			searchIndex = recipeInfo[0].nutrition.ingredients[a].name.indexOf(productName);
+			if (searchIndex != -1) {
+				ingredientIndex = searchIndex;
+				break;
+			}
+		}
+
+		// Get values from array
+		let units = recipeInfo[0].nutrition.ingredients[ingredientIndex].unit;
+
 		// Give it context and Style
-		tdQty.innerHTML = "<b>" + amount + "</b>";
+		tdQty.innerHTML = "<b>" + amount + " " + units + "</b>";
 		tdName.innerHTML = "<b>" + productName + "</b>";
 
 		// Appends to Page
@@ -314,8 +329,21 @@ function displayRecipe(event) {
 		let amount = recipeSelected.usedIngredients[i].amount;
 		let productName = recipeSelected.usedIngredients[i].name;
 
+		// Will be use to loop through the ingredients of the second API call
+		// to get the rest of the ingredients index
+		for (let a = 0; a < recipeInfo[0].nutrition.ingredients.length; a++) {
+			searchIndex = recipeInfo[0].nutrition.ingredients[a].name.indexOf(productName);
+			if (searchIndex != -1) {
+				ingredientIndex = searchIndex;
+				break;
+			}
+		}
+
+		// Get values from array
+		let units = recipeInfo[0].nutrition.ingredients[ingredientIndex].unit;
+
 		// Give it context
-		tdQty.textContent = amount;
+		tdQty.textContent = amount + " " + units;
 		tdName.textContent = productName;
 
 		// Appends to Page
@@ -325,12 +353,12 @@ function displayRecipe(event) {
 	}
 }
 //capitalize every word
-function capitalFormat (searchInput){
+function capitalFormat(searchInput) {
 	var cap = searchInput.split(" ");
 	for (let i = 0; i < cap.length; i++) {
-	 cap[i] = cap[i][0].toUpperCase() + cap[i].substr(1);
+		cap[i] = cap[i][0].toUpperCase() + cap[i].substr(1);
 	}
-	return(cap.join(' '));
+	return (cap.join(' '));
 }
 
 // Execute a function when the user presses a key on the keyboard while typing in the searchbox
