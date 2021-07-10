f115ba0ef6a8886aa73626e41912f88085b0ec32
Casey R <crowlandsdev@gmail.com>
2021-07-10 11:36:51 -0400

    fixed nav-dropdown display issue

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -3,16 +3,27 @@ const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 const dropdown = document.querySelector(".navbar-dropdown");
 const expandDropdownEl = document.querySelector(".navbar-link");
-const x = window.matchMedia("(max-width: 1280px)")
+const x = window.matchMedia("(max-width: 1024px)")
+const y = window.matchMedia("(min-width: 1025px)")
 
 burgerIcon.addEventListener("click", () => {
     console.log("clicked");
     navbarMenu.classList.toggle("is-active");
+    dropdown.setAttribute("class", "navbar-dropdown is-hidden");
 })
 
 expandDropdownEl.addEventListener("click", () => {
     console.log(dropdown.className);
     if(x.matches) {
+        if(dropdown.classList.contains("is-hidden")) {
+            dropdown.setAttribute("class", "navbar-dropdown is-active");
+        }
+        else {
+            dropdown.setAttribute("class", "navbar-dropdown is-hidden");
+        }
+    }
+
+    if(y.matches) {
         if(dropdown.classList.contains("is-hidden")) {
             dropdown.setAttribute("class", "navbar-dropdown is-active");
         }
