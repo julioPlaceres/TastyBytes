58057f74494b5f663a51328a6e8f4cbd94fee7da
Anthony Gress <the.techx.guy@gmail.com>
2021-07-11 13:07:03 -0400

    first live api call by ingredient working

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -102,6 +102,10 @@ function storeIngredients() {
 
 // Will search for a max of 5 recipes by ingredients
 function searchRecipeByIngredients() {
+	if (ingredients == ""){
+		alert("Add ingredients and then search for recipes")
+		return;
+	}
 	//function to create the url based on what is in the ingredients array
 		let apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=";
 		// loop through array of ingredients, add them to the url in the proper way
@@ -111,7 +115,6 @@ function searchRecipeByIngredients() {
 		// add the tail of the url after ingredients
 		apiUrl += "&number=5&ignorePantry=true&ranking=1";
 		// output the url for fetch request
-		console.log(apiUrl);
 	fetch(apiUrl, {
 		"method": "GET",
 		"headers": {
@@ -130,7 +133,13 @@ function searchRecipeByIngredients() {
 			// set global var to JSON object TODO rename
 					recipeData = data;
 			console.log(recipeData);
-			fillSuggestedRecipes(recipeData);
+			if (recipeData != ""){
+				fillSuggestedRecipes(recipeData);
+			}
+			else{
+				alert("No recipes found")
+				return;
+			}
 		})
 		// Error handler
 		.catch(function (err) {
@@ -160,14 +169,8 @@ function fillSuggestedRecipes() {
 
 function unhideRecipeList (){
 	let tile2 = document.querySelector(".right-tile");
-	// check if suggested list was hidden by selected recipe
-	if (recipeList.classList.contains("is-hidden")){
-		recipeList.classList.remove("is-hidden");
-		let recipeSelected = document.querySelector(".recipe-selected");
-		recipeSelected.classList.add("is-hidden");
-	}
 	// if tile 2 is hidden, unhide it
-	else if (tile2.classList.contains("is-hidden")){
+	if (tile2.classList.contains("is-hidden")){
 		tile2.classList.remove("is-hidden")
 	}
 }
@@ -273,136 +276,6 @@ function getRecipeInfo(recipeId) {
 		});
 	}
 
-
-
-//Display recipe
-// function displayRecipe(event) {
-// 	// if the target clicked is not an image will return
-// 	if (event.target.tagName.toLowerCase() != "img") {
-// 		return;
-// 	}
-
-// 	// Removes the current List of recipes
-// 	document.querySelector(".recipes-list").remove();
-
-// 	// Get the image that got cliked
-// 	let imageSelected = event.target;
-// 	// Retrieve Data Attribute assign to image
-// 	let id = imageSelected.dataset.id;
-
-// 	// Use to pass value from for loop and will
-// 	// contain the recipe selected
-// 	let recipeSelected;
-
-// 	// Loops through the recipe object array in order to get the ID and
-// 	// retrive the object for the main recipe
-// 	for (let i = 0; i < recipeData.length; i++) {
-// 		if (id == recipeData[i].id) {
-// 			recipeSelected = recipeData[i];
-// 		}
-// 	}
-
-// 	// Create the elements needed for the display of the recipe
-// 	let recipeName = document.createElement("h2");
-// 	let divImg1 = document.createElement("div");
-// 	let recipeImage = document.createElement("img");
-// 	let recipeSummary = document.createElement("p");
-
-// 	// Assign values to the elements and format them
-// 	recipeName.textContent = recipeSelected.title;
-// 	recipeName.setAttribute("class", "has-text-centered")
-// 	divImg1.setAttribute("class", "column");
-// 	recipeImage.setAttribute("src", recipeSelected.image);
-// 	recipeImage.setAttribute("class", "column");
-// 	recipeSummary.innerHTML = recipeInfo[0].summary;
-	
-// 	// Select the div that will hold the recipe selected and append to page
-// 	let recipeSelectedDiv = document.querySelector(".recipe-selected");
-// 	divImg1.append(recipeImage);
-// 	recipeSelectedDiv.append(recipeName);
-// 	recipeSelectedDiv.append(divImg1);
-// 	recipeSelectedDiv.append(recipeSummary);
-
-// 	// Removes the is-hidden class only from the selected recipe div
-// 	// and the ingredients list
-// 	recipeSelectedDiv.classList.remove("is-hidden");
-// 	ingredientsTable.classList.remove("is-hidden");
-
-// 	// Will display missed ingredients to the page (needs to be bold)
-// 	for (let i = 0; i < recipeSelected.missedIngredients.length; i++) {
-// 		// Creates a row
-// 		let trEl = document.createElement("tr");
-// 		let tdQty = document.createElement("td");
-// 		let tdName = document.createElement("td");
-
-// 		// Get values from array
-// 		let amount = recipeSelected.missedIngredients[i].amount;
-// 		let productName = recipeSelected.missedIngredients[i].name;
-
-// 		// Will be use to loop through the ingredients of the second API call
-// 		// to get the missing ingredient index
-// 		let ingredientIndex;
-// 		//Find the index of the ingredient missing
-// 		for (let a = 0; a < recipeInfo[0].nutrition.ingredients.length; a++) {
-// 			searchIndex = recipeInfo[0].nutrition.ingredients[a].name.indexOf(productName);
-// 			if (searchIndex != -1) {
-// 				ingredientIndex = a;
-// 				break;
-// 			}
-// 		}
-
-// 		// Get values from array
-// 		let units = recipeInfo[0].nutrition.ingredients[ingredientIndex].unit;
-
-// 		// Give it context and Style
-// 		tdQty.innerHTML = "<b>" + amount + " " + units + "</b>";
-// 		tdName.innerHTML = "<b>" + productName + "</b>";
-
-// 		// Appends to Page
-// 		trEl.appendChild(tdQty);
-// 		trEl.appendChild(tdName);
-// 		ingredientsTableBody.appendChild(trEl);
-// 	}
-
-// 	// Will display the current used ingredients
-// 	for (let i = 0; i < recipeSelected.usedIngredients.length; i++) {
-// 		// Creates a row
-// 		let trEl = document.createElement("tr");
-// 		let tdQty = document.createElement("td");
-// 		let tdName = document.createElement("td");
-
-// 		// Get values and format it
-// 		let amount = recipeSelected.usedIngredients[i].amount;
-// 		let productName = recipeSelected.usedIngredients[i].name;
-
-// 		// Will be use to loop through the ingredients of the second API call
-// 		// to get the rest of the ingredients index
-// 		for (let a = 0; a < recipeInfo[0].nutrition.ingredients.length; a++) {
-// 			searchIndex = recipeInfo[0].nutrition.ingredients[a].name.indexOf(productName);
-// 			if (searchIndex != -1) {
-// 				ingredientIndex = a
-// 				break;
-// 			}
-// 		}
-
-// 		// Get values from array
-// 		let units = recipeInfo[0].nutrition.ingredients[ingredientIndex].unit;
-
-// 		// Give it context
-// 		tdQty.textContent = amount + " " + units;
-// 		tdName.textContent = productName;
-
-// 		// Appends to Page
-// 		trEl.appendChild(tdQty);
-// 		trEl.appendChild(tdName);
-// 		ingredientsTableBody.appendChild(trEl);
-// 	}
-
-// 	// Add for loop
-// 	for (let i = 0; i < recipeSelected.usedIngredients.length; i++) {
-// 		// TODO : instructions (work in progress)
-// 	}
-// }
 //capitalize every word
 function capitalFormat(searchInput) {
 	var cap = searchInput.split(" ");
@@ -415,11 +288,8 @@ function capitalFormat(searchInput) {
 // Execute a function when the user presses a key on the keyboard while typing in the searchbox
 searchInputEl.addEventListener("keyup", onEnterKey);
 
-// Get target for photo selected
-// recipeList.addEventListener("click", displayRecipe);
-
 //TODO Will change the fill suggested recipes to searchRecipeByIngredients
-recipeBtn.addEventListener("click", fillSuggestedRecipes);
+recipeBtn.addEventListener("click", searchRecipeByIngredients);
 addIngredientBtn.addEventListener("click", handleSearchInput);
 ingredientList.addEventListener("click", removeIngredient);
 // Event listener for back button
